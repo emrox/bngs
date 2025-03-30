1ad0b2f5f5e52392a1e1f85158a1e315466c7c2f
Stefan Bauckmeier <github@elektronische-musik.de>
2025-03-30 14:02:52 +0200

    feat: initial bang redirect logic

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 import { Hono } from 'hono'
 
+import importBangFile from "./bang.json" with { type: "json" };
+const bangs: Record<string, string> = {};
+for (const importBang of importBangFile) {
+  bangs[importBang.t] = importBang.u
+}
+
+const bangRegex = new RegExp(`!([^\\s]+)`)
+
 const app = new Hono()
 
 app.get('/', (c) => {
@@ -9,7 +17,23 @@ app.get('/', (c) => {
     return c.redirect(`https://www.ecosia.org/`)
   }
 
-  return c.redirect(`https://www.ecosia.org/search?q=${encodeURI(searchQuery)}`)
+  if (!searchQuery.includes('!')) {
+    return c.redirect(`https://www.ecosia.org/search?q=${encodeURI(searchQuery)}`)
+  }
+
+  const bangMatch = searchQuery.match(bangRegex)
+  if (!bangMatch) {
+    return c.redirect(`https://www.ecosia.org/search?q=${encodeURI(searchQuery)}`)
+  }
+
+  const bang = bangMatch[1]
+  const bangUrl = bangs[bang]
+  if (!bangUrl) {
+    return c.redirect(`https://www.ecosia.org/search?q=${encodeURI(searchQuery)}`)
+  }
+
+  const bangSearchQuery = searchQuery.replace(`!${bang}`, '').trim()
+  return c.redirect(bangUrl.replace('{{{s}}}', encodeURI(bangSearchQuery)))
 })
 
 Deno.serve(app.fetch)
